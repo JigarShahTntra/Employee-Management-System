a7ac13e200149c8f24998e843a5f80afb240eec2
karmashivbaba <kunjprajapati555@EMAIL.com>
2021-07-19 17:09:51 +0530

    configuration bootstrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +13,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import 'bootstrap'
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require("packs/custom")
+require("packs/posts.js")
