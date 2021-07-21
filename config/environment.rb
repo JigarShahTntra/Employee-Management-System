6fcb6ece6c4e4136d34bd88ad97a1ef5f78afb0c
nidhi-1201 <pnidhi03@gmail.com>
2021-07-21 12:06:41 +0530

    modified environment and assest files

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings ={
+  address: 'smtp.gmail.com',
+  port: 465,
+  domain: 'gmail.com',
+  user_name: 'meets9921@gmail.com',
+  password: 'meet2604',
+  authentication: 'plain',
+  :ssl => true,
+  :tsl => true,
+  enable_starttles_auto: true
+}
