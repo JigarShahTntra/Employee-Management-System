9a342800f25c67a3c2c78bbb6550348aef7cb05a
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-22 11:39:54 +0530

    changes

--- db/migrate/20210721054004_create_leave_request_types.rb
+++ db/migrate/20210721054004_create_leave_request_types.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
+# CreateLeaveRequestTypes and table
 class CreateLeaveRequestTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :leave_request_types do |t|
       t.string :leaverequest_type
-
       t.timestamps
     end
   end
