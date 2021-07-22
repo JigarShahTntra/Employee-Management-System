2c685b6d4cc21eddd9cd6b624311ec7301092b75
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-22 17:45:25 +0530

    changes

--- app/assets/javascript/form_validation.js
+++ app/assets/javascript/form_validation.js
@@ -1,9 +1,14 @@
 FormValidation = {
   validateForm: function () {
+    jQuery.validator.addMethod("lettersonly", function (value, element) {
+      return this.optional(element) || /^[a-z]+$/g.test(value);
+    }, " small-letters only please");
+
     $('#leave_request_type-form').validate({
       rules: {
         "leave_request_type[leaverequest_type]": {
-          required: true
+          required: true,
+          lettersonly: true
         }
       }
     });
--- app/models/leave_request_type.rb
+++ app/models/leave_request_type.rb
@@ -3,4 +3,6 @@
 # LeaveRequestType model
 class LeaveRequestType < ApplicationRecord
   validates :leaverequest_type, presence: true
+  validates :leaverequest_type, format: { with: /\A[a-z]+\z/,
+                                          message: 'only allows small letters' }
 end
