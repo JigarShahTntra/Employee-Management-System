41be471faa101f61278729af474316d8ede6373b
meet <mj.thakkar97@gmail.com>
2021-07-22 13:07:37 +0530

    roles changes

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,6 @@
 # class/role/model
 class Role < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :name, format: { with: /\A[a-z]+\z/, message: 'only allows small letters' }
+  validates :name, format: { with: /\A[a-z]+\z/,
+                             message: 'only allows small letters' }
 end
