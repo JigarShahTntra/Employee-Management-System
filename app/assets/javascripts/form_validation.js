16477b45d791efbbcdfb04ccadaeec46caa89e06
meet <mj.thakkar97@gmail.com>
2021-07-22 12:42:59 +0530

    roles page changes

--- app/assets/javascripts/form_validation.js
+++ app/assets/javascripts/form_validation.js
@@ -1,15 +1,15 @@
 FormValidation = {
   validateForm: function () {
-    jQuery.validator.addMethod("lettersonly", function(value, element) {
+    jQuery.validator.addMethod("lettersonly", function (value, element) {
       return this.optional(element) || /^[a-z]+$/g.test(value);
-    }, "lowercase and Letters only please");      
+    }, "lowercase and Letters only please");
     $('#role-form').validate({
       rules: {
         "role[name]": {
           required: true,
           lettersonly: true
         }
-      }  
+      }
     });
   }
-}
+};
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,15 +12,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_093922) do
-
+ActiveRecord::Schema.define(version: 202_107_190_939_22) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
