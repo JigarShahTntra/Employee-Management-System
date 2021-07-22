48ed99b96f4ad4d63a057e706b279325650a81ee
meet <mj.thakkar97@gmail.com>
2021-07-22 12:35:44 +0530

    roles page changes

--- db/migrate/20210719093922_create_roles.rb
+++ db/migrate/20210719093922_create_roles.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
+# app/migrate/db/roles/
 class CreateRoles < ActiveRecord::Migration[6.1]
   def change
     create_table :roles do |t|
       t.string :name
-
       t.timestamps
     end
   end
