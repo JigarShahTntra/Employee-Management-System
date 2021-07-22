f82c173726b388760e92b206229a25154f46a586
HarshRajput9520 <rajputharsh7999@gmail.com>
2021-07-22 12:15:17 +0530

    hound changes 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require("jquery")
+require("jquery");
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
