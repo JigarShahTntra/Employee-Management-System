6ebf56158376ce853b209ae154fb483a03a0d4fc
meet <mj.thakkar97@gmail.com>
2021-07-24 12:56:17 +0530

    department validation and formating

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("jquery")
+require("jquery");
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 import 'bootstrap'
-require("packs/form_validation.js")
-require("packs/js_cdn.js")
-require("packs/validation.js")
