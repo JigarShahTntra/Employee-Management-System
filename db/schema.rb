ddf1491ab9313eff0ffb993526f49b99fe52d448
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-24 13:21:38 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_054004) do
-
+ActiveRecord::Schema.define(version: 20_210_721_054_004) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "leave_request_types", force: :cascade do |t|
-    t.string "leaverequest_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'leave_request_types', force: :cascade do |t|
+    t.string 'leaverequest_type'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "leavetypes", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'leavetypes', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
