11c410e8a26a07eabd355f35dbc14a533caec582
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-26 12:23:32 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -8,21 +8,21 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control system.
+# It"s strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_721_054_004) do
+ActiveRecord::Schema.define(version: 2021_07_21_054004) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'leave_request_types', force: :cascade do |t|
-    t.string 'leaverequest_type'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "leave_request_types", force: :cascade do |t|
+    t.string "leaverequest_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'leavetypes', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "leavetypes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 end
