295464d2628b7676607e4434e3c47a5bb541fc98
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-28 17:45:48 +0530

    changes

--- test/controllers/leaves_request_types_controller_test.rb
+++ test/controllers/leaves_request_types_controller_test.rb
@@ -1,7 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+# module of LeavesRequestTypesController
+require 'test_helper'
 
 class LeavesRequestTypesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get leaves_request_types_index_url
     assert_response :success
   end
--- test/models/leave_request_type_test.rb
+++ test/models/leave_request_type_test.rb
@@ -1,4 +1,7 @@
-require "test_helper"
+# frozen_string_literal: true
+
+# module of LeaveRequestTypeTest
+require 'test_helper'
 
 class LeaveRequestTypeTest < ActiveSupport::TestCase
   # test "the truth" do
