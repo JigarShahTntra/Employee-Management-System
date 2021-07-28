58103d8bdcfaf15db5cf22a7b97d66bf355a2d89
Kunj555 <kunjprajapati555@EMAIL.com>
2021-07-28 14:02:45 +0530

    feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'feedbacks/index'
+  root'feedbacks#index'
+  resources:feedbacks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
